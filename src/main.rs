0cb61a7f67fd0a5871804103c84d23e92288f7a2
mohanrajendran <lynermohan@gmail.com>
2016-06-08 09:29:36 -0500

    Implemented memory

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,31 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
-fn read_file(file_name: &String) -> std::io::Result<Vec<u16>>{
+fn read_file(file_name: &String, memory: &mut [u16]) -> std::io::Result<()>{
     let mut f = try!(File::open(file_name.to_string()));
     let mut buffer = Vec::new();
 
     let size = try!(f.read_to_end(&mut buffer))/2;
 
-    let mut result: Vec<u16> = Vec::new();
-
     for i in 0..size {
         let bit1 = buffer[i*2] as u16;
         let bit2 = buffer[i*2+1] as u16;
 
-        let bit: u16 = bit1 + (bit2 << 8);
+        let cell: u16 = bit1 + (bit2 << 8);
 
-        result.push(bit);
+        memory[i] = cell;
     }
     
-    Ok(result)
+    Ok(())
 }
 
-fn execute(program: &Vec<u16>, pc: usize) -> Option<usize> {
-    let op = program[pc];
+fn execute(memory: &mut [u16], pc: usize) -> Option<usize> {
+    let op = memory[pc];
 
     if op == 0 {
         None 
     } else if op == 19 {
-        print!("{}", program[pc+1] as u8 as char);
+        print!("{}", memory[pc+1] as u8 as char);
         Some(pc + 2)
     } else if op == 21 {
         Some(pc + 1)
@@ -45,11 +43,13 @@ fn main() {
         return;
     }
 
-    let program = read_file(&args[1]).expect("Unable to read program");
+    let mut memory: [u16; 32768] = [0; 32768];
+
+    read_file(&args[1], &mut memory).expect("Unable to read program");
 
     let mut pc: usize = 0;
 
     loop {
-        pc = execute(&program, pc).expect("Halting code");
+        pc = execute(&mut memory, pc).expect("Halting code");
     }
 }
